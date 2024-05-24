69c46b48326446edcfea487bffdf989a696dfb2f
niccolocorraro <nic.corraro@stud.uniroma3.it>
2024-05-24 17:32:13 +0200

    check grafico

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,15 @@
 
 
-insert into game (id,name,category,release_date) values (nextval('games_seq'),'Minecraft','Adventure',2008);
-insert into game (id,name,category,release_date) values (nextval('games_seq'),'The last of Us','Adventure',2010);
-insert into game (id,name,category,release_date) values (nextval('games_seq'),'Destiny','Adventure',2015);
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'Minecraft','Adventure',2008,'questa è la descrizione di minecraft');
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'The last of Us','Adventure',2010,'questa è la descrizione di tlou');
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'Destiny','Adventure',2015,'questa è la descrizione di destiny');
 
-insert into game (id,name,category,release_date) values (nextval('games_seq'),'Minecraft','Adventure',2008);
-insert into game (id,name,category,release_date) values (nextval('games_seq'),'The last of Us','Adventure',2010);
-insert into game (id,name,category,release_date) values (nextval('games_seq'),'Destiny','Adventure',2015);
 
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'Minecraft','Adventure',2008,'questa è la descrizione di minecraft');
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'The last of Us','Adventure',2010,'questa è la descrizione di tlou');
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'Destiny','Adventure',2015,'questa è la descrizione di destiny');
+
+
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'Minecraft','Adventure',2008,'questa è la descrizione di minecraft');
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'The last of Us','Adventure',2010,'questa è la descrizione di tlou');
+insert into game (id,name,category,release_date,description) values (nextval('games_seq'),'Destiny','Adventure',2015,'questa è la descrizione di destiny');
