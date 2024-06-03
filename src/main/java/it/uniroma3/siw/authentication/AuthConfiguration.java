762f7519f9678ce6830dfaebe9c49f57844e5b9c
FrancescoFontanesi <fra.fontanesi@stud.uniroma3.it>
2024-06-03 16:01:43 +0200

    Authentication Completed

--- src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -1,6 +1,4 @@
 package it.uniroma3.siw.authentication;
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +7,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.service.CustomUserDetailsService;
@@ -20,64 +20,64 @@ import it.uniroma3.siw.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
-public class AuthConfiguration {
+public class AuthConfiguration  {
 
-    @Autowired
-    private DataSource dataSource;
+   
     
     @Autowired
     CustomUserDetailsService customUserDetailsSerivce;
 
     @SuppressWarnings("deprecation")
 	  @Bean PasswordEncoder passwordEncoder() {
-		  //return new BCryptPasswordEncoder();
+		 //return new BCryptPasswordEncoder();
 		  return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	  
 	  }
-	 
     
- 
 
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { 
+    	        auth.userDetailsService(customUserDetailsSerivce).passwordEncoder(passwordEncoder()); 
+    	}
     
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		/*
-		 * auth.jdbcAuthentication() .dataSource(dataSource)
-		 * .authoritiesByUsernameQuery("SELECT email, role from credentials WHERE email=?"
-		 * )
-		 * .usersByUsernameQuery("SELECT email, password, 1 as enabled FROM credentials WHERE username=?"
-		 * ) .passwordEncoder(passwordEncoder());
-		 */
-    	
-    	auth.userDetailsService(customUserDetailsSerivce);
-    	//.passwordEncoder(passwordEncoder());
-    }
+    
+    
 
     @Bean
     @Order(1)
-    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/**", "/index", "/register", "/css/**", "/images/**", "favicon.ico").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/register", "/login").permitAll()
-                                .requestMatchers("/admin/**").hasAuthority(Credentials.ADMIN_ROLE)
-                                .anyRequest().authenticated()
-                )
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/login")
-                                .permitAll()
-                                .defaultSuccessUrl("/", true)
-                                .failureUrl("/login?error")
-                )
-                .logout(logout ->
-                        logout
-                                .logoutUrl("/logout")
-                                .logoutSuccessUrl("/")
-                                .invalidateHttpSession(true)
-                                .deleteCookies("JSESSIONID")
-                );
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/", "/index", "/register", "/css/**", "/images/**", "favicon.ico").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/register", "/login").permitAll()
+                    .requestMatchers("/admin/**").hasAuthority(Credentials.ADMIN_ROLE)
+                    .anyRequest().authenticated())
+            .formLogin(formLogin ->
+                formLogin
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/", true)
+                    .failureUrl("/login?error"))
+            .logout(logout ->
+                logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID"))
+            .sessionManagement(sessionManagement ->
+                sessionManagement
+                    .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+            .anonymous(anonymous -> anonymous.disable());
 
         return http.build();
     }
+
+    @Bean
+    HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
+
+
+
 }
--- src/main/java/it/uniroma3/siw/controller/HomeController.java
+++ src/main/java/it/uniroma3/siw/controller/HomeController.java
@@ -2,10 +2,16 @@ package it.uniroma3.siw.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.model.User;
+import it.uniroma3.siw.repository.CredentialsRepository;
 import it.uniroma3.siw.repository.GameRepository;
 
 @Controller
@@ -13,12 +19,37 @@ public class HomeController {
 	
 	@Autowired
 	public GameRepository gameRepository;
+	@Autowired
+	private CredentialsRepository credentialsRepository;
+	
 
     @GetMapping(value = "/")
     public String index(Model model) {
-    	model.addAttribute("games", gameRepository.findAll());
-        return "index.html"; 
-    }
+    	 model.addAttribute("games", gameRepository.findAll());
+
+         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+         if (authentication != null && authentication.isAuthenticated()) {
+             Object principal = authentication.getPrincipal();
+             if (principal instanceof UserDetails) {
+                 UserDetails userDetails = (UserDetails) principal;
+                 String email = userDetails.getUsername();
+                 System.out.println("Authenticated user email: " + email);
+
+                 Credentials credentials = credentialsRepository.findByEmail(email)
+                     .orElseThrow(() -> new RuntimeException("User not found"));
+
+                 User user = credentials.getUser();
+                 System.out.println(user.toString());
+                 model.addAttribute("userPrincipal", user);
+             } else {
+                 System.out.println("Principal is not an instance of UserDetails");
+             }
+         } else {
+             model.addAttribute("userPrincipal", null);
+             System.out.println("No authenticated user");
+         }
+         return "index";
+     }
     
     @GetMapping("/cart")
 	public String getCart(Model model) {
